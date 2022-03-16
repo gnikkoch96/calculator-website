3843814e0b4da025157a765f7706146ee3bbf96b
Nikko <gnikkoch@gmail.com>
2022-03-16 16:34:16 -0700

    add function definition to add, subtract, multiply, divide, and operate

--- js/index.js
+++ js/index.js
@@ -1,4 +1,37 @@
-function add(num1, num2){}
-function subtract(num1, num2){}
-function multiply(num1, num2){}
-function divide(num1, num2){}
+let calcDisplay = document.querySelector('.calculator-display')
+calcDisplay.textContent = '0';
+
+// op will represent the symbol of the operation
+function operate(op, num1, num2){
+    switch(op){
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    if(num2 == 0) return 'IDIOT';
+    return num1/num2;
+}
+
