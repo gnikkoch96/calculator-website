9b50669d058f2721f688623ccbb034b4798390e1
Nikko <gnikkoch@gmail.com>
2022-03-17 13:02:34 -0700

    add secondary display to show the full expression

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ const MAX_NUM_DISPLAY = 12;
 let calcDisplay = document.querySelector('.calculator-display')
 calcDisplay.textContent = '0';
 
+let calcDisplayOverall = document.querySelector('.calculator-overall-display');
+
+console.log(calcDisplayOverall)
+
 let buttons = document.querySelectorAll('.calculator-buttons button');
 addButtonEventListeners(buttons);
 
@@ -12,42 +16,93 @@ let pressedOperator = false;
 
 // 0 by default
 let num1 = 0, num2 = 0;
+let operator = '';
 
 function addButtonEventListeners(buttons){
     buttons.forEach(button => {     
         // number pad listener
         if(button.className == 'number-btn'){
             button.addEventListener('click', () => {
-                if(calcDisplay.textContent === '0') 
+                if(calcDisplay.textContent === '0' || pressedOperator) {
                     calcDisplay.textContent = button.textContent;
-                else 
+                    pressedOperator = false;
+                }else 
                     calcDisplay.textContent += button.textContent;
             }); 
         }       
 
+        // dot
+        if(button.id === 'dot'){
+            button.addEventListener('click', () => {
+                if(!pressedDot){
+                    calcDisplay.textContent += button.textContent;        
+
+                    pressedDot = true;
+                }
+            });
+        }
+
         // operator listeners
         if(button.className === 'operator'){
             button.addEventListener('click', () => {
-                // store num1
-                num1 = Number(calcDisplay.textContent);
-                
-                // toggle flag
-                pressedOperator = true;
+                if(!pressedOperator){
+                    // store num1
+                    num1 = Number(calcDisplay.textContent);
+    
+                    // toggle flag
+                    pressedOperator = true;
+                }
+
+                operator = button.textContent;
+
+                // update overall display
+                calcDisplayOverall.textContent = `${num1} ${operator}`;
             });
         }
 
-        // clear
-        if(button.id === 'clear'){
-            button.addEventListener('click', () => {
-                //reset all flags
-                pressedDot = false;
-                pressedOperator = false;
+        if(button.id === 'equals'){
+            button.addEventListener('click', equals);
+        }
 
-                // reset display
-                calcDisplay.textContent = '0';
-            });
+        if(button.id === 'clear'){
+            button.addEventListener('click', clear);
         }
     });
+}
+
+function clear(){
+    //reset all flags
+    pressedDot = false;
+    pressedOperator = false;
+
+    //reset all vars
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+
+    // reset display
+    calcDisplay.textContent = '0';
+}
+
+function equals(){
+    // store num2
+    num2 = Number(calcDisplay.textContent);
+
+    //update overall display
+    calcDisplayOverall.textContent = `${calcDisplayOverall.textContent} ${num2} =`;
+
+    // calculate and append to display 
+    let result = operate(operator, num1, num2);
+    console.log(result);
+
+    calcDisplay.textContent = result;
+
+    // store to num1;
+    num1 = result;
+
+    // reset flags
+    pressedDot = false;
+    pressedOperator = false;
 }
 
 // op will represent the symbol of the operation
